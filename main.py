6082035030972600a48a4dbaefbbc0b3884c0809
ahmed dacharoui <dachraouia193@gmail.com>
2024-10-13 17:19:36 +0100

    add check_face function

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 counter = 0
 face_match = False
-reference_img = cv2.imread("reference.jpg")
+reference_img = cv2.imread('reference.png')
 
 
 def check_face(frame):
-    pass
+    global face_match
+    try:
+        if DeepFace.verify(frame, reference_img.copy())["verified"]:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
 
 
 while True:
@@ -27,6 +34,10 @@ while True:
                 pass
         counter += 1
 
+        if face_match:
+            cv2.putText(frame, "Match", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "No Match", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
